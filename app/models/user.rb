f64eeffe7f5da686ddfdda4000ff43b317d428c9
taniw <tani.s.wong@gmail.com>
2015-02-12 17:15:43 -0800

    Implemented BCrypt authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,33 @@
+require 'bcrypt'
+
 class User < ActiveRecord::Base
+ BCrypt::Engine.cost = 12
+
+  attr_reader :password
+
+  validates_confirmation_of :password
+  validates_presence_of :password_digest
+
+  def authenticate(unencrypted_password)
+    if BCrypt::Password.new(password_digest) == unencrypted_password
+      self
+    else
+      false
+    end
+  end
+
+  def password=(unencrypted_password)
+    if unencrypted_password.nil?
+      self.password_digest = nil
+    else 
+      @password = unencrypted_password
+      self.password_digest = BCrypt::Password.create(@password)
+    end
+  end
+
+  def self.confirm(username_param, password_param)
+  	user = User.find_by({username: username_param})
+  	user.authenticate(password_param)
+  end
+
 end
