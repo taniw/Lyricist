7aad608157c72b3424bde077f528ae6ab90867b4
taniw <tani.s.wong@gmail.com>
2015-02-17 11:45:00 -0800

    Added comments to application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,15 +16,19 @@
 //= require_tree .
 
 $(function(){
+	// setting the url to http://localhost:3000 for whichever browser I use
 	var BASE_PATH = "http://" + ( window.location.host || document.location.host );
 
 
 
 
 	$('.search-song').click(function() {
+		// take the value of what's typed in the search box
 		var q = $('.search-query').val();
+		// trim the white spaces front and back of the searched query
 		var search_by = $('.search-by-options-btn').text().trim();
 
+		// the search query has to be equal or longer than 3 characters to proceed
 		if (q.length >= 3) {
 			$.ajax({
 				url: BASE_PATH + "/search",
@@ -33,9 +37,13 @@ $(function(){
 					q: q
 				},
 				success: function(response) {
+					// get the contents of that searched element
 					var source   = $("#song-search-results-tpl").html();
+					// grabbing template from index-home when search results pop up and showing them on page
 					var template = Handlebars.compile(source);
+					// append the lyrics to the selected song
 					$('.search-results').empty().append( template({data: response}) );
+					// hit this function (can show and hide song lyrics)
 					toggleSongLyricsOnClick();
 				},
 				error: function(response) {
@@ -46,12 +54,17 @@ $(function(){
 	});
 
 	function toggleSongLyricsOnClick(){
+		// when you click on one of the search results
 		$('.results-list li').click(function(e){
+			// click the closest targeted li and get that specific song id
 			var song_id = $(e.target).closest('li').attr('id');
+			// shows the lyrics of the song by id
 			var lyrics_div = $('.lyrics-' + song_id);
 
+			// if lyrics is empty, be able to toggle and return the ajax call to show lyrics
 			if( ! lyrics_div.is(':empty') ) {
 				lyrics_div.toggle();
+
 				return;
 			}
 
